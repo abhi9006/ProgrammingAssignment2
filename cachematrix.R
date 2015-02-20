ace741d508812ad749ba25a60e4dfc1c02bb7047
abhi9006 <abhi9006@users.noreply.github.com>
2015-02-20 06:07:18 -0800

    matrix<-x$get()
    matrix is a function and  solve need a matrix.
    ## so we need to change matrix<-x$get to matrix<-x$get()

--- cachematrix.R
+++ cachematrix.R
@@ -1,26 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function, makeVector creates a special "vector", which is really a list containing a function to
+## set the value of the vector
+## get the value of the vector
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
- m<-NULL
-        set<-function(y){
-                x<<-y
-                m<<-NULL
+ m <- NULL
+ ## set the value of the vector
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
         }
-        get<-function() x
-        setmatrix<-function(solve) m<<- solve
-        getmatrix<-function() m
-        list(set=set, get=get,
-             setmatrix=setmatrix,
-             getmatrix=getmatrix)
-
+ ## get the value of the vector
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
 }
-
-
-## Write a short comment describing this function
-
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         cacheSolve <- function(x=matrix(), ...) {
@@ -29,7 +28,9 @@ cacheSolve <- function(x, ...) {
                 message("getting cached data")
                 return(m)
         }
-        matrix<-x$get
+ ## matrix is a function and  solve need a matrix.
+ ## so we need to change matrix<-x$get to matrix<-x$get()
+        matrix<-x$get()
         m<-solve(matrix, ...)
         x$setmatrix(m)
         m
